27466e55ec7100eff8624a45656f6824b478f4b4
daddychukz <durugo_chuks@yahoo.com>
2021-01-02 08:51:06 +0100

    Added comments

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,14 @@ LABEL application="MkDocs Tool"
 
 RUN apk update
 
+#install dependencies
 RUN apk add --no-cache tar gcc gzip musl-dev && pip install mkdocs 
 
 WORKDIR /MkDocs_tool
 
 COPY mkdockerize.sh utils.sh /MkDocs_tool/scripts/
 
+# make script executable
 RUN chmod 755 /MkDocs_tool/scripts/mkdockerize.sh
 
 EXPOSE 8000
--- mkdockerize.sh
+++ mkdockerize.sh
@@ -2,24 +2,29 @@
 
 source ${PWD}/scripts/utils.sh
 
+# Function to produce/build the website
 produce() {
 
+	#validate if local path is supplied in argument
     if [[ -z "$1" ]]; then
 		error "Enter path to the local directory that contains the Mkdocs project."
 	fi
 
+	#validate project exists before building website
     if [[ -d $1 ]]; then
 		cd $1
 		mkdocs build
+		echo "dev_addr: 0.0.0.0:8000" >> mkdocs.yml
 		tar -czvf project.tar.gz .
 		mv project.tar.gz ../
+		rm -rf site
 		success "Build Succeeded..."
 	fi
 }
 
+# Function to run the website
 serve() {
 	cd /MkDocs_tool/my-site
-	rm rf site
 	mkdir project
 	cat project.tar.gz | tar xzf - -C project
 	cd project

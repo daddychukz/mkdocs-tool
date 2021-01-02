690f93c4b31dc9d4f423a535b788977f1cf843f1
daddychukz <durugo_chuks@yahoo.com>
2021-01-02 11:12:23 +0100

    refactor scripts

--- produce.sh
+++ produce.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+#variables
+container_mount_root=""
+container_name=""
+mkdocs_project_dir=""
+image_name=""
+
 #remove container
-docker container rm <container name>
+docker container rm $container_name
 
 rm -rf project project.tar.gz &&
-docker run -it -v <local mkdocs folder>:<container mount point> --name <container name> mkdocs produce <container mount point> &&
+docker run -it -v ${mkdocs_project_dir}:${container_mount_root}/my-project --name ${container_name} $image_name produce ${container_mount_root}/my-project &&
 
 #copy created tar file
-docker cp <container name>:/MkDocs_tool/project.tar.gz . 
+docker cp ${container_name}:/${container_mount_root}/project.tar.gz . 
--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run --rm -p 8000:8000 -it -v ${PWD}:/MkDocs_tool/my-site --name mkdocs-site mkdocs serve
+image_name=
+
+docker run --rm -p 8000:8000 -it -v ${PWD}:/MkDocs_tool/my-site --name mkdocs-site $image_name serve
